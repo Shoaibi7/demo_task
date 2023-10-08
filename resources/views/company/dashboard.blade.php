71eb66ab85a45e282eb188ddf9086de9bbdb9cbb
Muhammad Shoaib <m.shoaib2k15@gmail.com>
2023-10-08 16:48:19 +0500

    added changes

--- resources/views/company/dashboard.blade.php
+++ resources/views/company/dashboard.blade.php
@@ -43,9 +43,7 @@
                 <div class="mt-16">
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
                     
-
                       <H1>Hello Guys</H1>
-
                         
                     </div>
                 </div>
